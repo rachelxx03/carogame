b1893b6c2b164a468d1a29b91689adc4ed02c15a
rachelxx03 <Ltu123@456b>
2022-04-30 14:16:22 -0300

    newupdateformenu

--- carogame/game.py
+++ carogame/game.py
@@ -2,7 +2,7 @@ import os.path
 import button
 
 import pygame
-WIDTH,HEIGHT=900,900
+WIDTH,HEIGHT=750,750
 WIN=pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("caro")
 gray=(36,36,36)
@@ -12,13 +12,13 @@ start_img=pygame.transform.scale(pygame.image.load(os.path.join("art", "start.pn
 quit_img=pygame.transform.scale(pygame.image.load(os.path.join("art", "quit.png")),(950,1000)).convert_alpha()
 
 
-start_button = button.Button(-320,-30 , start_img, 1)
-exit_button = button.Button(270, -130, quit_img, 1)
+start_button = button.Button(-250,-30 , start_img, 0.8)
+exit_button = button.Button(220, -110, quit_img, 0.8)
 def main():
    run=True
    while run:
         WIN.blit(background,(0,0))
-        WIN.blit(logo, (60, 0))
+        WIN.blit(logo, (-10, 0))
 
         if start_button.draw(WIN):
             print('START')
